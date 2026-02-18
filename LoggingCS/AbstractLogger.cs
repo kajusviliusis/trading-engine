74082281f031a5a6f6c7b9e8c9ce8498e9c0cfff
kajusviliusis <kajus.viliusis0@gmail.com>
2026-02-18 22:02:30 +0200

    feat: abstract logger

--- LoggingCS/AbstractLogger.cs
+++ LoggingCS/AbstractLogger.cs
@@ -1,6 +1,19 @@
 namespace TradingEngineServer.Logging;
 
-abstract class AbstractLogger : ILogger
+public abstract class AbstractLogger : ILogger
 {
-    protected abstract Log(LogLevel logLevel, string module, string message);
+    protected AbstractLogger() { }
+    protected abstract void Log(LogLevel logLevel, string module, string message);
+    
+    public void Debug(string module, string message) => Log(LogLevel.Debug, module, message);
+    public void Debug(string module, Exception exception) => Log(LogLevel.Debug, module, $"{exception}");
+
+    public void Information(string module, string message) => Log(LogLevel.Information, module, message);
+    public void Information(string module, Exception exception) => Log(LogLevel.Information, module, $"{exception}");
+
+    public void Warning(string module, string message) => Log(LogLevel.Warning, module, message);
+    public void Warning(string module, Exception exception) => Log(LogLevel.Warning, module, $"{exception}");
+
+    public void Error(string module, string message) => Log(LogLevel.Error, module, message);
+    public void Error(string module, Exception exception) => Log(LogLevel.Error, module, $"{exception}");
 }
