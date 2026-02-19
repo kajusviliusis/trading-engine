fba048cd994b9882e03ac286ed4ff95d8603da7e
kajusviliusis <kajus.viliusis0@gmail.com>
2026-02-19 19:28:47 +0200

    feat: logAsync task and log formatting

---
+++ LoggingCS/LogInformation.cs
@@ -0,0 +1,3 @@
+namespace TradingEngineServer.Logging;
+
+public record LogInformation(LogLevel LogLevel, string Module, string Message, DateTime Now, int ThreadId, string ThreadName);
--- LoggingCS/TextLogger.cs
+++ LoggingCS/TextLogger.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks.Dataflow;
 using Microsoft.Extensions.Options;
 
 namespace TradingEngineServer.Logging.LoggingConfiguration;
@@ -8,14 +10,48 @@ public class TextLogger : AbstractLogger, ITextLogger
     public TextLogger(IOptions<LoggerConfiguration> loggingConfiguration) : base()
     {
         _loggerConfiguration = loggingConfiguration.Value ??  throw new ArgumentNullException(nameof(loggingConfiguration));
+        string filepath = string.Empty;
+        _ = Task.Run(() => LogAsync(filepath, _logQueue, _tokenSource.Token));
     }
+
+    private static async Task LogAsync(string filepath, BufferBlock<LogInformation> logQueue, CancellationToken token)
+    {
+        using var fs = new FileStream(filepath, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+        using var sw = new StreamWriter(fs);
+
+        try
+        {
+            while (true)
+            {
+                var logItem = logQueue.ReceiveAsync(token).ConfigureAwait(false);
+                string formattedMessage = FormatLogItem(logItem);
+                await sw.WriteAsync(formattedMessage).ConfigureAwait(false);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+    }
+
+    private static string FormatLogItem(LogInformation logItem)
+    {
+        return $"[{logItem.Now:f}] [{logItem.ThreadName}:{logItem.ThreadId:000}] " + 
+               $"[{logItem.LogLevel}] {logItem.Message}";
+    }
+
     protected override void Log(LogLevel logLevel, string module, string message)
     {
-        throw new NotImplementedException();
+        _logQueue.Post(new LogInformation(logLevel, module, message, DateTime.Now,
+            Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.Name));
     }
 
     public void Dispose()
     {
         throw new NotImplementedException();
     }
+    
+    private BufferBlock<LogInformation> _logQueue = new BufferBlock<LogInformation>();
+    private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
 }
